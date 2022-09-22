468094a392d731b1b8dc36b6938aa327407be69a
Pratik Pansare <84313956+pratik97669966@users.noreply.github.com>
2022-09-22 22:37:17 +0530

    Update call.js

--- call.js
+++ call.js
@@ -10,8 +10,9 @@ remoteVideo.onplaying = () => { remoteVideo.style.opacity = 1 }
 let peer
 function init(userId) {
     peer = new Peer(userId, {
-         host:'opentalkapp.herokuapp.com', secure:true, port:443
- /*      
+        host:'opentalkvoice.herokuapp.com', secure:true, port:443
+ /*     
+   host:'opentalkvoice.herokuapp.com', secure:true, port:443
   host:'peerjs-server.herokuapp.com', secure:true, port:443
    host:'opentalkapp.herokuapp.com', secure:true, port:443
       
